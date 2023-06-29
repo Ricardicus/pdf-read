d57c7961ca48df489babbd175c9fb42094b3b915
Rickard Hallerbäck <rickard.hallerback@epiroc.com>
2023-06-29 13:03:12 +0200

    answers loops

--- generate_answers_loop.sh
+++ generate_answers_loop.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
 
+# Check if $1 is defined
+if [ -z "$1" ]; then
+    echo "usage: $0 document"
+    exit 1
+fi
 
 counter=0
 while [ $counter -lt 100 ];
 do
-    python generate_answers.py --document outputs/TIS0004144-output.txt --questions outputs/questions_condensed_again.json --index outputs/indexes.json
+    python generate_answers.py --document $1 --questions outputs/questions_condensed_again.json --index outputs/indexes.json
     counter=$((counter+1))
     sleep 1
 done
